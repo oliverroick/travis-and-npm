5de3c0ddfa44171ffe59c8b05c69a09dbf13542d
Oliver Roick <oliver.roick@gmail.com>
2019-01-26 13:18:16 +0100

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
-cp .npmrc $HOME/.npmrc
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+echo "$PACKAGE_VERSION; $TRAVIS_TAG"
 
-if [ "$PACKAGE_VERSION" != "$TRAVIS_TAG" ]
-then
-    echo "Git and NPM do not match"; exit 1;
-fi;
+[ "$PACKAGE_VERSION" != "$TRAVIS_TAG" ] && echo "Git and NPM do not match" && exit 1;
 
-RELEASE_TAG="latest"
-if [[ $PACKAGE_VERSION =~ .*alpha.* || $PACKAGE_VERSION =~ .*beta.* ]]; then
-    RELEASE_TAG="dev-$PACKAGE_VERSION"
-fi;
+echo "Deploying"
 
-npm publish --tag=$RELEASE_TAG
+# RELEASE_TAG="latest"
+# if [[ $PACKAGE_VERSION =~ .*alpha.* || $PACKAGE_VERSION =~ .*beta.* ]]; then
+#     RELEASE_TAG="dev-$PACKAGE_VERSION"
+# fi;
+
+# npm publish --tag=$RELEASE_TAG
